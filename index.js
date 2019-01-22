87a230b04ff3c8da4f20c994defbf66c05289c15
Giancarlo Galliani <giancarlo.pecchia@arcadiagroup.co.uk>
2019-01-22 11:23:37 +0000

    Add index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function tendon(dataHash) {
+  // console.log(dataHash)
+  // console.log(dataHash.toJSON())
+  window.test = {
+    data: dataHash,
+    update: () => {
+    }
+  };
+  return test;
+}
