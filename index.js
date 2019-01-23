122ddcf7062a844eb0592251900e82330276cdd8
Giancarlo Galliani <giancarlo.pecchia@arcadiagroup.co.uk>
2019-01-23 12:15:39 +0000

    Add methods and bower.json

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+console.log('Tendon module available in global scope')
 const tendon = function(dataHash) {
   this.dataHash = dataHash
   console.log('Tendon init')
   window.test = {
     data: dataHash
   }
-
-  return test
 }
 
-tendon.prototype.update = function(key, value) {
-  this.dataHash.set(key, value)
+tendon.prototype.update = function(obj) {
+  // Too specific? 
+  this.dataHash.get('containers').add(obj)
+}
+
+tendon.prototype.fetch = function(key) { 
+  return this.dataHash.get(key).toJSON()
+}
+
+tendon.prototype.fetchModel = function() {
+  return this.dataHash
 }
