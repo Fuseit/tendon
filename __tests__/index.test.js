28bfdcfe035be89bfb808a8f25aea22047db7ff9
Gabriel Hernandez <ghernandez345@gmail.com>
2019-01-31 12:40:57 +0000

    added test utils and test for toRawData

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,3 +1,4 @@
+import MockUtils from './mockUtils';
 import Tendon from '../src/index';
 import * as Events from '../src/topics';
 
@@ -11,7 +12,7 @@ beforeEach(() => {
     subscribe: jest.fn()
   };
 
-  onMock = jest.fn();
+  onMock = jest.fn(); // used to spy on the 'on' backbone coollection method
   mockModel = {
     get: () => ({
       on: onMock
@@ -36,23 +37,42 @@ describe('Tendon instance', () => {
 
   describe('.toRawData() method', () => {
     beforeEach(() => {
-      mockPubSub = {
-        subscribe: jest.fn()
-      };
-
       onMock = jest.fn();
       mockModel = {
-        get: key => ({
-          on: onMock,
-          toJSON: () => ({ bar: 2 })
-        }),
-        toJSON: () => ({ foo: 1 })
+        toJSON: () => ({ foo: 1 }),
+        get: key => {
+          if (key === 'containers') {
+            return {
+              on: onMock,
+              toJSON: () => [{ bar: 2 }]
+            };
+          }
+          return {
+            toJSON: () => [{ baz: 3 }]
+          };
+        }
       };
 
       tendon = new Tendon(mockPubSub, mockModel);
     });
 
     test('correctly marshalls a backbone data structure to a raw JS object', () => {
+      // setup of the mocked backbone model and what data it should contain.
+      const modelData = { foo: 1 };
+      const containersData = [{ bar: 2 }];
+      const availableWidgetsData = [{ baz: 3 }];
+
+      // this is the data we are passing into the backbone mock and what we will
+      // expect in a raw data in the assertion.
+      const mockModelData = {
+        ...modelData,
+        containers: containersData,
+        available_widgets: availableWidgetsData
+      };
+
+      mockModel = MockUtils.generateMockBackboneModel(mockModelData);
+      tendon.model = mockModel;
+
       expect(tendon.toRawData()).toEqual({
         foo: 1,
         containers: [{ bar: 2 }],
---
+++ __tests__/mockUtils.js
@@ -0,0 +1,42 @@
+const defaultModelData = {
+  containers: [],
+  available_widgets: []
+};
+
+const defaultMocks = {
+  onMock: () => {}
+};
+
+/**
+ * Generates an object that mocks a backbone model. Takes in dynamic data and
+ * mock functions to enable custom mock behaviour per instance.
+ * @param {Object} mockData Data contained inside this mock backbone model.
+ * @param {Object} mocks    Object of mocks that allows for custom mock behaviour per mock instance
+ */
+const generateMockBackboneModel = (
+  mockData = defaultModelData,
+  mocks = defaultMocks
+) => {
+  return {
+    toJSON: () => {
+      return {
+        ...mockData
+      };
+    },
+
+    get: key => {
+      // TODO: make this more dynamic.
+      if (key === 'containers') {
+        return {
+          // TODO: add on Mock
+          toJSON: () => mockData.containers
+        };
+      }
+      return {
+        toJSON: () => mockData.available_widgets
+      };
+    }
+  };
+};
+
+export default { generateMockBackboneModel };
